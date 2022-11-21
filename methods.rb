62d9551162c2b8d2a676ced37b0e6b3a0e8c8f29
maxnmcbride@gmail.com <maxnmcbride@gmail.com>
2022-11-21 10:10:30 -0500

    commit tree 622ca9ab9778275930eaf13711a7cd9ce50be14e

--- methods.rb
+++ methods.rb
@@ -1,1 +1,25 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+greet('Max')
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+greet_with_default("Jane")
+
+    
+def add (a, b)
+    a + b
+end
+
+def halve(x)
+    if x.class != Integer
+        return nil
+    end
+    x / 2
+end
